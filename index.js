2aa5c742cbc819217ebd87cb3cbbd136bfdb39ba
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-09-18 15:34:21 +0600

    Update-for-inspect-mode

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ console.log("Hello");
 console.log("index js detected");
 const texts= document.getElementsByTagName('h1');
 for(let text of texts)console.log(text.innerText);
+console.log("QuerySelectorAll Start");
+const rhq=document.querySelectorAll('#rhq');
+for(let text of rhq)console.log(text.innerText);

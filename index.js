ccb29930c51ce3f9b4f18bd3dfc3012c39b00852
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-09-18 16:09:40 +0600

    update

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ for(let text of texts)console.log(text.innerText);
 console.log("QuerySelectorAll Start");
 const rhq=document.querySelectorAll('#rhq');
 for(let text of rhq)console.log(text.innerText);
+// style in js
+document.getElementById('arizona').style.textAlign='center'
+document.getElementById('arizona').style.fontSize='45px';
+document.getElementById('arizona').style.fontWeight='bold';
+document.getElementById('arizona').style.color='brown';
+
